8112957ee04d4de3632ccc33bbaecfae2efffa8c
Ethosa <social.ethosa@gmail.com>
2024-04-06 17:06:17 +0700

    update package

---
+++ elys.nimble
@@ -0,0 +1,9 @@
+# Package
+
+description = "Little scripting language"
+author = "HapticX"
+version = "0.2.0"
+license = "MIT"
+srcDir = "src"
+installExt = @["nim"]
+bin = @["elys"]
--- src/elys.nim
+++ src/elys.nim
@@ -1,8 +1,13 @@
 import
-  ./modules/lexer,
-  ./modules/parser,
-  ./modules/ast,
+  ./elyspkg/lexer,
+  ./elyspkg/parser,
+  ./elyspkg/ast,
   options
+
+
+when isMainModule:
+  import
+    terminal
 
 
 proc exec*(code: string) =
@@ -12,3 +17,18 @@ proc exec*(code: string) =
   if parsed.isSome:
     var env = newEnv()
     discard parsed.get.ast.eval(env)
+
+
+when isMainModule:
+  const VERSION = "0.2.0"
+
+  styledEcho fgRed, "      _           "
+  styledEcho fgRed, "  ___| |_   _ ___ "
+  styledEcho fgRed, " / _ \\ | | | / __|"
+  styledEcho fgRed, "|  __/ | |_| \\__ \\"
+  styledEcho fgRed, " \\___|_|\\__, |___/"
+  styledEcho fgRed, "        |___/     "
+  echo ""
+
+  styledEcho fgMagenta, "Elys scripting language ", fgYellow, "v", VERSION, fgMagenta, " by HapticX"
+
